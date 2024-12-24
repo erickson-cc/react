cb566642bc39c1097eb9d66a4860492a37073086
erickson-cc <e.rickson@live.com>
2024-12-24 00:12:18 -0300

    Project 2 - Mount, Unmount, Update

--- project-2/App.js
+++ project-2/App.js
@@ -3,19 +3,49 @@ import { Component } from 'react';
 
 class App extends Component{
 	state = {
+		counter: 0,
 		posts: [
 			{ id: 1,
+			title: 'blank',
+			body: 'blank',
+			},
+			{ id: 2,
+			title: 'blank',
+			body: 'blank',
+			},
+			{ id: 3,
 			title: 'blank',
 			body: 'blank',
 			},
 
 		]
 	};
+	componentDidMount(){
+		this.handleTimeout();
+	}
+	componentDidUpdate(){
+		this.handleTimeout();
+	}
+	componentWillUnmount() {
+		clearTimeout(this.timeoutUpdate);
+	}
+	timeoutUpdate = null;
+	handleTimeout = () => {
+		const {posts,counter} = this.state;
+		posts[0].title = "O título mudou";
+		this.timeoutUpdate = setTimeout(() => {
+			this.setState({
+				posts,
+				counter: counter+1
+			})
+		}, 1)
+	}
 	render(){
 		//const name = this.state.name;
-		const { posts } = this.state;//destructuring
+		const {posts,counter} = this.state;//destructuring
 		return(
 			<div className="App">
+				<h1>{counter}</h1>
 				{posts.map(post => (
 					<div key={post.id}>
 						<h1>{post.title}</h1>
